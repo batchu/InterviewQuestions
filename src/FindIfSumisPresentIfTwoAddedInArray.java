0210322c281775fa4d161bbc2aba80ce0d10fed3
Prashanth Batchu <me@prashu.net>
2016-12-11 22:34:34 -0500

    Working on solution

---
+++ src/FindIfSumisPresentIfTwoAddedInArray.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by me on 12/11/2016.
+ */
+public class FindIfSumisPresentIfTwoAddedInArray {
+
+    public static void main(String[] args){
+
+
+
+    }
+
+    public static int[][] findIfSumisPresentIfTwoAddedInArray(int[] array){
+
+        HashMap<Integer, Integer> eleMap = new HashMap<>();
+
+        for(int i=0; i<array.length;i++){
+            eleMap.put(i,array[i]);
+        }
+        HashMap< ArrayList<Integer>, Integer> solution = new HashMap<>();
+
+        for(int i=0;i<array.length;i++){
+
+            for(int j=i;j<array.length;j++)
+            {
+
+            }
+        }
+        return null;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -7,28 +7,30 @@ public class Main {
         System.out.println("Hello World!");
 
         List<Integer> stream = new ArrayList<Integer>();
+        List<Integer> newStream = new ArrayList<Integer>();
 
-        for(int i=0;i<20;i++){
+        for (int i = 0; i < 20; i++) {
 
-                stream.add(i+1);
+            stream.add(i + 1);
         }
 
 
-        int j=2;
-        int k=1;
-        //need to update the termination condition
-        List<Integer> newStream= new ArrayList<Integer>();
-        while(k<20){
-            if((k-1)<stream.size() && k%j != 0){//Every 3rd element should be true
-                newStream.add(k);
+//j is which jth element to be removed for each loop. For first pass, 2nd elements are removed. For second pass, 3rd elements are removed
+        for (int j = 2, k = 0; j < 8; j++) {
+            k = 0;
+            while (k < 20) {
+                if ((k - 1) < stream.size() && k % j != 0) {//Every 3rd element should be true
+                    newStream.add(stream.get(k - 1));
+                }
+                k++;
+
             }
-            k++;
+            for (int i : stream) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            stream = newStream;
+            newStream = new ArrayList<Integer>();
         }
-
-        for(int i:newStream)
-        {
-            System.out.println(i);
-        }
-
     }
 }
